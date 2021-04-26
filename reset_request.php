762bd29d9cf9069ee1c15cd9101262f5e02332ba
CYNTHIAGICHUKI <gichukicynthia1@gmail.com>
2021-04-27 01:12:36 +0300

    This is the functionality of reseting the password

---
+++ reset_request.php
@@ -0,0 +1,68 @@
+<?php
+
+if(isset($_POST["reset-request-submit"]))
+{
+
+  $selector = bin2hex(random_bytes(8));
+  $token = random_bytes(32);
+
+  $url = "www.mmtuts.net/forgottenpwd/create-new-password.php?selector=" . $selector . "&validator=" .bin2hex($token);
+
+  $expires = date("U") + 1800;
+
+  require 'db_connect.php';
+
+  $userEmail =$_POST["email"];
+
+
+  $sql = "DELETE FROM pwdReset WHERE pwdResetEmail =?";
+  $stmt = mysqli_stmt_init($conn);
+
+  if(!mysqli_stmt_prepare($stmt, $sql)){
+    echo "There was an error!";
+    exit();
+  }
+    else {
+      mysqli_stmt_bind_param($stmt, "s", $userEmail);
+      mysqli_stmt_execute($stmt);
+    }
+$sql = "INSERT INTO pwdReset (pwdResetEmail, pwdResetSelector, pwdResetToken, pwdResetExpires ) VALUES (?, ?, ?, ?);";
+
+$stmt = mysqli_stmt_init($conn);
+
+if(!mysqli_stmt_prepare($stmt, $sql)){
+  echo "There was an error!";
+  exit();
+}
+  else {
+    $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+    mysqli_stmt_bind_param($stmt, "s", $userEmail, $hashedToken, $expires);
+    mysqli_stmt_execute($stmt);
+  }
+
+mysqli_stmt_close($stmt);
+mysqli_close();
+
+
+$to = $userEmail;
+
+$subject = "Reset your password for mtuts";
+
+$message = '<p> We received a password reset request. The link to reset your password is here below. If you did not make this request you can ignore this email</p>';
+$message .= '<p>Here is your password reset link: <br>';
+$message .= '<a href="' . $url . '">'. $url . '</a></p>';
+
+$headers = "From: cynthia <gichukicynthia1@gmail.com>\r\n";
+$headers .= "Reply-To: cynthia <gichukicynthia1@gmail.com>";
+$headers .= "Content-type: text/html\r\n";
+
+mail($to, $subject, $message, $headers);
+header("Location:forgot-password.php?reset=success");
+
+}
+
+
+else{
+  header("Location:index.php");
+}
+?>
